08217f2ff40cc255a1cf4431c3ddd40ec69b8353
Pedro Henrique Pinto da Silva <pitterller@gmail.com>
2021-01-20 23:43:04 -0300

    corrigindo ex19

--- Exercicios/lista_04/ex19.py
+++ Exercicios/lista_04/ex19.py
@@ -1,14 +1,14 @@
 # /bin/env python
 # -*- encode: utf-8 -*-
 __author__ = '@pedro1hen1'
-#Exercicio 19
+# Exercicio 19
 """Faça um programa que receba o valor de uma dívida e mostre uma tabela com os seguintes dados: valor da dívida,
 valor dos juros, quantidade de parcelas e valor da parcela.
 Os juros e a quantidade de parcelas seguem a tabela abaixo:
 Quantidade de Parcelas  % de Juros sobre o valor inicial da dívida
 1       0
 3       10
-6       15
+6       17
 9       20
 12      25
 Exemplo de saída do programa:
@@ -17,37 +17,42 @@ R$ 1.000,00     0               1                       R$  1.000,00
 R$ 1.100,00     100             3                       R$    366,00
 R$ 1.150,00     150             6                       R$    191,67
 """
-divida = float(input("Insira o valor da sua divida atual para consultar tabela: "))
-parcela = 1
 
-print("Valor da divida: ", end="  ")
-print("Valor do juros: ", end="  ")
-print("Quantidade de parcelas: ", end="  ")
-print("Valor da parcela: ")
+def ex19():
+    divida = float(input("Insira o valor da sua divida atual para consultar tabela: "))
+    parcela = 1
 
-for i in range(5):
-    if parcela == 1:
-        juros = 1
-        v_juros = 0
-    elif parcela == 4:
-        parcela = 3
-        juros = 1.10
-    elif parcela == 7 or parcela == 6:
-        parcela = 6
-        juros = 1.15
-    elif parcela == 10 or parcela == 9:
-        parcela = 9
-        juros = 1.20
-    elif parcela == 13 or parcela == 12:
-        parcela = 12
-        juros = 1.25
+    print("Valor da divida: ", end="  ")
+    print("Valor do juros: ", end="  ")
+    print("Quantidade de parcelas: ", end="  ")
+    print("Valor da parcela: ")
 
-    v_juros = divida * (juros - 1)
-    divida_com_juros = divida * juros
-    valor_parcela = divida_com_juros / parcela
+    for i in range(5):
+        if parcela == 1:
+            juros = 1
+            v_juros = 0
+        elif parcela == 4:
+            parcela = 3
+            juros = 1.10
+        elif parcela == 7 or parcela == 6:
+            parcela = 6
+            juros = 1.15
+        elif parcela == 10 or parcela == 9:
+            parcela = 9
+            juros = 1.20
+        elif parcela == 13 or parcela == 12:
+            parcela = 12
+            juros = 1.25
 
-    print("R$", round(divida_com_juros, 2), end="            ")
-    print(round(v_juros, 2), end="                  ")
-    print(parcela, end="                        ")
-    print("R$ ", round(valor_parcela, 2))
-    parcela += 3
+        v_juros = divida * (juros - 1)
+        divida_com_juros = divida * juros
+        valor_parcela = divida_com_juros / parcela
+
+        print("R$", round(divida_com_juros, 2), end="            ")
+        print(round(v_juros, 2), end="                  ")
+        print(parcela, end="                        ")
+        print("R$ ", round(valor_parcela, 2))
+        parcela += 3
+
+
+ex19()
